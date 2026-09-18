613f0323ddb301e6a96ee07ae527e3836e7f69e5
pca2242-creator <pca2242@gmail.com>
2026-09-18 14:26:29 +0500

    Update server.js

--- server.js
+++ server.js
@@ -123,4 +123,7 @@ function resetGame() {
     room = { players: [], deck: [], discardPile: [], currentTurn: 0, started: false };
 }
 
-server.listen(3000, () => console.log('Server 3000-portda ishlamoqda...'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
